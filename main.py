c583cdd025af79a5bb597e56286067f0b8039456
Mark Dube <mjdube99@gmail.com>
2021-09-04 14:55:23 -0500

    be able to accept client connection and log it

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
-# import SimpleHTTPServer
-# import SocketServer
-import os
 import logging
-import sys
+import socket
 import readOptions
 
-# Port that's open
-PORT = readOptions.getOptions('options_honeypot', 'port')
-print(f"Port: {PORT}")
-
-# Pretty sure this doesn't work with syslog FIX: change later
+# Pretty sure this doesn"t work with syslog FIX: change later
+# Sets up logging settings
 logging.basicConfig(
-    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level = logging.INFO,
-    filename = 'honeypot.log',
+    filename = "honeypot.log",
 )
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Local IP/Port for the honeypot to listen on (TCP)
+LOCAL_HOST = '0.0.0.0'
+# Gets port from options.ini file (should be able to switch between ssh and telnet)
+LOCAL_PORT = int(readOptions.getOptions("options_honeypot", "port"))
+
+# Remote IP/Port to send the log data to (TCP)
+# FIX:
+LOG_HOST = ""
+LOG_PORT = 9000
+
+# Banner displayed when connecting to the honeypot
+BANNER = readOptions.getOptions("options_honeypot", "banner")
+# Socket timeout in seconds
+TIMEOUT = 3
+
+
+# TODO: Rename s
+def main():
+    # Sets up the socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((LOCAL_HOST, LOCAL_PORT))
+    # Waiting for connection
+    s.listen(5)
+    while True:
+        clientsocket, clientaddress = s.accept() # FIX: Doesn't get past this line
+        s.settimeout(TIMEOUT)
+        logger.info(f"{clientsocket} has connected at {clientaddress}")
+        print(f"Honeypot has connection from {clientaddress}")
+        try:
+            clientsocket.send(bytes(BANNER, "utf-8"))
+            msg = s.recv(1024)
+        except socket.error:
+            logger.critical(f"Error: {str(socket.error)}")
+        else:
+            logger.critical(f"Data: {msg}")
+        finally:
+            s.close()
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
